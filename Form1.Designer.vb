9eb47b10bfe7f9b93dc81860c8d3c93dc5b3b1dc
GerryCV1007 <gerrycv10@gmail.com>
2025-10-17 19:22:21 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,177 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Panel1 = New Panel()
+        btnlogin = New Button()
+        LinkLabel1 = New LinkLabel()
+        PictureBox3 = New PictureBox()
+        PictureBox2 = New PictureBox()
+        TextBox2 = New TextBox()
+        TextBox1 = New TextBox()
+        Label1 = New Label()
+        PictureBox1 = New PictureBox()
+        Panel1.SuspendLayout()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackColor = Color.FromArgb(CByte(230), CByte(236), CByte(245))
+        Panel1.Controls.Add(btnlogin)
+        Panel1.Controls.Add(LinkLabel1)
+        Panel1.Controls.Add(PictureBox3)
+        Panel1.Controls.Add(PictureBox2)
+        Panel1.Controls.Add(TextBox2)
+        Panel1.Controls.Add(TextBox1)
+        Panel1.Controls.Add(Label1)
+        Panel1.Controls.Add(PictureBox1)
+        Panel1.Location = New Point(309, 67)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(630, 738)
+        Panel1.TabIndex = 0
+        ' 
+        ' btnlogin
+        ' 
+        btnlogin.BackColor = Color.FromArgb(CByte(59), CByte(130), CByte(246))
+        btnlogin.BackgroundImageLayout = ImageLayout.None
+        btnlogin.FlatAppearance.BorderSize = 0
+        btnlogin.FlatStyle = FlatStyle.Flat
+        btnlogin.Font = New Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnlogin.ForeColor = Color.FromArgb(CByte(31), CByte(41), CByte(55))
+        btnlogin.Location = New Point(153, 594)
+        btnlogin.Name = "btnlogin"
+        btnlogin.Size = New Size(351, 54)
+        btnlogin.TabIndex = 6
+        btnlogin.Text = "Login"
+        btnlogin.UseVisualStyleBackColor = False
+        ' 
+        ' LinkLabel1
+        ' 
+        LinkLabel1.AutoSize = True
+        LinkLabel1.LinkColor = Color.FromArgb(CByte(37), CByte(99), CByte(235))
+        LinkLabel1.Location = New Point(431, 532)
+        LinkLabel1.Name = "LinkLabel1"
+        LinkLabel1.Size = New Size(146, 25)
+        LinkLabel1.TabIndex = 5
+        LinkLabel1.TabStop = True
+        LinkLabel1.Text = "Forgot Password"
+        ' 
+        ' PictureBox3
+        ' 
+        PictureBox3.BackColor = SystemColors.ActiveCaption
+        PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), Image)
+        PictureBox3.Location = New Point(62, 479)
+        PictureBox3.Name = "PictureBox3"
+        PictureBox3.Size = New Size(55, 50)
+        PictureBox3.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox3.TabIndex = 4
+        PictureBox3.TabStop = False
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.BackColor = SystemColors.ButtonHighlight
+        PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), Image)
+        PictureBox2.Location = New Point(62, 400)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(55, 50)
+        PictureBox2.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox2.TabIndex = 1
+        PictureBox2.TabStop = False
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.BackColor = Color.WhiteSmoke
+        TextBox2.Font = New Font("Segoe UI Semibold", 16F, FontStyle.Bold Or FontStyle.Italic)
+        TextBox2.Location = New Point(62, 479)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(515, 50)
+        TextBox2.TabIndex = 3
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.BackColor = Color.WhiteSmoke
+        TextBox1.Font = New Font("Segoe UI Semibold", 16F, FontStyle.Bold Or FontStyle.Italic)
+        TextBox1.Location = New Point(62, 400)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(515, 50)
+        TextBox1.TabIndex = 2
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Engravers MT", 20F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(39, 292)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(562, 47)
+        Label1.TabIndex = 1
+        Label1.Text = "BARANGAY PORTAL"
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackColor = Color.FromArgb(CByte(230), CByte(236), CByte(245))
+        PictureBox1.ErrorImage = Nothing
+        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
+        PictureBox1.Location = New Point(195, 64)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(236, 199)
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ButtonFace
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        BackgroundImageLayout = ImageLayout.Stretch
+        ClientSize = New Size(1253, 889)
+        Controls.Add(Panel1)
+        DoubleBuffered = True
+        MaximizeBox = False
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Form1"
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents btnlogin As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        MessageBox.Show("Logging you in, please wait...", "Please Wait", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Threading.Thread.Sleep(2000)
+        MessageBox.Show("Login successful!", "Welcome")
+    End Sub
+
+End Class
