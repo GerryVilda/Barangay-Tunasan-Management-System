818372a1af134fb6c75e2bcc88c8b80b955ea16e
GerryCV1007 <gerrycv10@gmail.com>
2025-10-20 23:05:29 +0800

    module

---
+++ Tunasan_Module.vb
@@ -0,0 +1,15 @@
+﻿Imports MySql.Data.MySqlClient
+
+Module Connection
+    Public cn As New MySqlConnection("server=localhost;user=root;password=;database=tunasan_management_db")
+    Public cmd As MySqlCommand
+    Public dr As MySqlDataReader
+    Public da As MySqlDataAdapter
+    Public dt As DataTable
+
+    Public Sub ConnectDB()
+        If cn.State = ConnectionState.Closed Then
+            cn.Open()
+        End If
+    End Sub
+End Module
