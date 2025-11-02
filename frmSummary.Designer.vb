abe4328d8f39cccfa39f377da913c7955fc8fe27
JULIANA MARIE DAMICOG <C1071-23@itmlyceumalabang.onmicrosoft.com>
2025-11-02 17:46:48 +0800

    frmSummary.vb

--- frmSummary.Designer.vb
+++ frmSummary.Designer.vb
@@ -23,57 +23,453 @@ Partial Class frmSummary
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmSummary))
-        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New DataVisualization.Charting.ChartArea()
-        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New DataVisualization.Charting.Legend()
-        Dim Series1 As System.Windows.Forms.DataVisualization.Charting.Series = New DataVisualization.Charting.Series()
         PictureBox1 = New PictureBox()
-        Chart1 = New DataVisualization.Charting.Chart()
+        lblTitle = New Label()
+        Panel1 = New Panel()
+        btnGenerate = New Button()
+        dtpTo = New DateTimePicker()
+        dtpFrom = New DateTimePicker()
+        Label3 = New Label()
+        Label2 = New Label()
+        Label1 = New Label()
+        cmbSitio = New ComboBox()
+        Panel2 = New Panel()
+        lblHouseholds = New Label()
+        lblTotalResidents = New Label()
+        lblSitioName = New Label()
+        Label7 = New Label()
+        Label6 = New Label()
+        Label5 = New Label()
+        Label4 = New Label()
+        MySqlCommand1 = New MySql.Data.MySqlClient.MySqlCommand()
+        Panel3 = New Panel()
+        lblSenior = New Label()
+        lblAdulthood = New Label()
+        lblChildhood = New Label()
+        lblFemale = New Label()
+        lblMale = New Label()
+        Label16 = New Label()
+        Label15 = New Label()
+        Label14 = New Label()
+        Label12 = New Label()
+        Label11 = New Label()
+        Label13 = New Label()
+        Label10 = New Label()
+        Label9 = New Label()
         CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
-        CType(Chart1, ComponentModel.ISupportInitialize).BeginInit()
+        Panel1.SuspendLayout()
+        Panel2.SuspendLayout()
+        Panel3.SuspendLayout()
         SuspendLayout()
         ' 
         ' PictureBox1
         ' 
         PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
-        PictureBox1.Location = New Point(12, 12)
+        PictureBox1.Location = New Point(2, 0)
+        PictureBox1.Margin = New Padding(2)
         PictureBox1.Name = "PictureBox1"
-        PictureBox1.Size = New Size(216, 189)
+        PictureBox1.Size = New Size(173, 151)
         PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
         PictureBox1.TabIndex = 43
         PictureBox1.TabStop = False
         ' 
-        ' Chart1
+        ' lblTitle
         ' 
-        ChartArea1.Name = "ChartArea1"
-        Chart1.ChartAreas.Add(ChartArea1)
-        Legend1.Name = "Legend1"
-        Chart1.Legends.Add(Legend1)
-        Chart1.Location = New Point(289, 141)
-        Chart1.Name = "Chart1"
-        Series1.ChartArea = "ChartArea1"
-        Series1.Legend = "Legend1"
-        Series1.Name = "Series1"
-        Chart1.Series.Add(Series1)
-        Chart1.Size = New Size(973, 517)
-        Chart1.TabIndex = 45
-        Chart1.Text = "Chart1"
+        lblTitle.AutoSize = True
+        lblTitle.Font = New Font("Segoe UI Black", 19.8000011F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        lblTitle.Location = New Point(180, 18)
+        lblTitle.Name = "lblTitle"
+        lblTitle.Size = New Size(318, 46)
+        lblTitle.TabIndex = 44
+        lblTitle.Text = "Reports Summary"
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(128))
+        Panel1.Controls.Add(btnGenerate)
+        Panel1.Controls.Add(dtpTo)
+        Panel1.Controls.Add(dtpFrom)
+        Panel1.Controls.Add(Label3)
+        Panel1.Controls.Add(Label2)
+        Panel1.Controls.Add(Label1)
+        Panel1.Controls.Add(cmbSitio)
+        Panel1.Location = New Point(180, 87)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(955, 100)
+        Panel1.TabIndex = 45
+        ' 
+        ' btnGenerate
+        ' 
+        btnGenerate.BackColor = Color.SandyBrown
+        btnGenerate.FlatAppearance.BorderSize = 0
+        btnGenerate.FlatStyle = FlatStyle.Flat
+        btnGenerate.Font = New Font("Segoe UI", 12F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        btnGenerate.ForeColor = Color.Black
+        btnGenerate.Location = New Point(830, 14)
+        btnGenerate.Name = "btnGenerate"
+        btnGenerate.Size = New Size(110, 36)
+        btnGenerate.TabIndex = 6
+        btnGenerate.Text = "Generate"
+        btnGenerate.UseVisualStyleBackColor = False
+        ' 
+        ' dtpTo
+        ' 
+        dtpTo.CalendarFont = New Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        dtpTo.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        dtpTo.Location = New Point(488, 16)
+        dtpTo.Name = "dtpTo"
+        dtpTo.Size = New Size(325, 34)
+        dtpTo.TabIndex = 5
+        ' 
+        ' dtpFrom
+        ' 
+        dtpFrom.CalendarFont = New Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        dtpFrom.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        dtpFrom.Location = New Point(79, 61)
+        dtpFrom.Name = "dtpFrom"
+        dtpFrom.Size = New Size(325, 34)
+        dtpFrom.TabIndex = 4
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label3.Location = New Point(437, 15)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(45, 31)
+        Label3.TabIndex = 3
+        Label3.Text = "To:"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label2.Location = New Point(3, 61)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(76, 31)
+        Label2.TabIndex = 2
+        Label2.Text = "From:"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(3, 19)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(70, 31)
+        Label1.TabIndex = 1
+        Label1.Text = "Sitio:"
+        ' 
+        ' cmbSitio
+        ' 
+        cmbSitio.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        cmbSitio.FormattingEnabled = True
+        cmbSitio.Items.AddRange(New Object() {"Camella Homes III", "Lake Shore Subdivision", "Lindenwood Residences", "Midland II Subdivision", "Parkhomes Subdivision", "Planas", "Sto. Niño Village", "Susana Heights Subdivision", "Villa Carolina I", "Villa Carolina II"})
+        cmbSitio.Location = New Point(79, 14)
+        cmbSitio.Name = "cmbSitio"
+        cmbSitio.Size = New Size(325, 36)
+        cmbSitio.TabIndex = 0
+        ' 
+        ' Panel2
+        ' 
+        Panel2.BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(128))
+        Panel2.Controls.Add(lblHouseholds)
+        Panel2.Controls.Add(lblTotalResidents)
+        Panel2.Controls.Add(lblSitioName)
+        Panel2.Controls.Add(Label7)
+        Panel2.Controls.Add(Label6)
+        Panel2.Controls.Add(Label5)
+        Panel2.Controls.Add(Label4)
+        Panel2.Location = New Point(12, 231)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(563, 442)
+        Panel2.TabIndex = 46
+        ' 
+        ' lblHouseholds
+        ' 
+        lblHouseholds.AutoSize = True
+        lblHouseholds.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lblHouseholds.Location = New Point(247, 198)
+        lblHouseholds.Name = "lblHouseholds"
+        lblHouseholds.Size = New Size(27, 31)
+        lblHouseholds.TabIndex = 56
+        lblHouseholds.Text = "0"
+        ' 
+        ' lblTotalResidents
+        ' 
+        lblTotalResidents.AutoSize = True
+        lblTotalResidents.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lblTotalResidents.Location = New Point(247, 147)
+        lblTotalResidents.Name = "lblTotalResidents"
+        lblTotalResidents.Size = New Size(27, 31)
+        lblTotalResidents.TabIndex = 55
+        lblTotalResidents.Text = "0"
+        ' 
+        ' lblSitioName
+        ' 
+        lblSitioName.AutoSize = True
+        lblSitioName.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lblSitioName.Location = New Point(247, 98)
+        lblSitioName.Name = "lblSitioName"
+        lblSitioName.Size = New Size(27, 31)
+        lblSitioName.TabIndex = 54
+        lblSitioName.Text = "0"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold)
+        Label7.Location = New Point(94, 198)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(147, 31)
+        Label7.TabIndex = 49
+        Label7.Text = "Households:"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold)
+        Label6.Location = New Point(60, 147)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(181, 31)
+        Label6.TabIndex = 48
+        Label6.Text = "Total Residents:"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI", 18F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        Label5.Location = New Point(154, 15)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(259, 41)
+        Label5.TabIndex = 47
+        Label5.Text = "Sitio Information"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold)
+        Label4.Location = New Point(102, 98)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(139, 31)
+        Label4.TabIndex = 7
+        Label4.Text = "Sitio Name:"
+        ' 
+        ' MySqlCommand1
+        ' 
+        MySqlCommand1.CacheAge = 0
+        MySqlCommand1.Connection = Nothing
+        MySqlCommand1.EnableCaching = False
+        MySqlCommand1.Transaction = Nothing
+        ' 
+        ' Panel3
+        ' 
+        Panel3.BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(128))
+        Panel3.Controls.Add(lblSenior)
+        Panel3.Controls.Add(lblAdulthood)
+        Panel3.Controls.Add(lblChildhood)
+        Panel3.Controls.Add(lblFemale)
+        Panel3.Controls.Add(lblMale)
+        Panel3.Controls.Add(Label16)
+        Panel3.Controls.Add(Label15)
+        Panel3.Controls.Add(Label14)
+        Panel3.Controls.Add(Label12)
+        Panel3.Controls.Add(Label11)
+        Panel3.Controls.Add(Label13)
+        Panel3.Controls.Add(Label10)
+        Panel3.Controls.Add(Label9)
+        Panel3.Location = New Point(581, 231)
+        Panel3.Name = "Panel3"
+        Panel3.Size = New Size(554, 442)
+        Panel3.TabIndex = 55
+        ' 
+        ' lblSenior
+        ' 
+        lblSenior.AutoSize = True
+        lblSenior.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lblSenior.Location = New Point(243, 324)
+        lblSenior.Name = "lblSenior"
+        lblSenior.Size = New Size(27, 31)
+        lblSenior.TabIndex = 61
+        lblSenior.Text = "0"
+        ' 
+        ' lblAdulthood
+        ' 
+        lblAdulthood.AutoSize = True
+        lblAdulthood.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lblAdulthood.Location = New Point(243, 279)
+        lblAdulthood.Name = "lblAdulthood"
+        lblAdulthood.Size = New Size(27, 31)
+        lblAdulthood.TabIndex = 60
+        lblAdulthood.Text = "0"
+        ' 
+        ' lblChildhood
+        ' 
+        lblChildhood.AutoSize = True
+        lblChildhood.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lblChildhood.Location = New Point(243, 236)
+        lblChildhood.Name = "lblChildhood"
+        lblChildhood.Size = New Size(27, 31)
+        lblChildhood.TabIndex = 59
+        lblChildhood.Text = "0"
+        ' 
+        ' lblFemale
+        ' 
+        lblFemale.AutoSize = True
+        lblFemale.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lblFemale.Location = New Point(116, 148)
+        lblFemale.Name = "lblFemale"
+        lblFemale.Size = New Size(27, 31)
+        lblFemale.TabIndex = 58
+        lblFemale.Text = "0"
+        ' 
+        ' lblMale
+        ' 
+        lblMale.AutoSize = True
+        lblMale.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lblMale.Location = New Point(116, 101)
+        lblMale.Name = "lblMale"
+        lblMale.Size = New Size(27, 31)
+        lblMale.TabIndex = 57
+        lblMale.Text = "0"
+        ' 
+        ' Label16
+        ' 
+        Label16.AutoSize = True
+        Label16.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold)
+        Label16.Location = New Point(49, 324)
+        Label16.Name = "Label16"
+        Label16.Size = New Size(188, 31)
+        Label16.TabIndex = 57
+        Label16.Text = "Senior (60-80+):"
+        ' 
+        ' Label15
+        ' 
+        Label15.AutoSize = True
+        Label15.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold)
+        Label15.Location = New Point(2, 279)
+        Label15.Name = "Label15"
+        Label15.Size = New Size(235, 31)
+        Label15.TabIndex = 56
+        Label15.Text = "Adulthood (18-50+):"
+        ' 
+        ' Label14
+        ' 
+        Label14.AutoSize = True
+        Label14.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold)
+        Label14.Location = New Point(19, 236)
+        Label14.Name = "Label14"
+        Label14.Size = New Size(218, 31)
+        Label14.TabIndex = 55
+        Label14.Text = "Childhood (0-13+):"
+        ' 
+        ' Label12
+        ' 
+        Label12.AutoSize = True
+        Label12.Font = New Font("Segoe UI", 18F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        Label12.Location = New Point(116, 15)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(316, 41)
+        Label12.TabIndex = 47
+        Label12.Text = "Resident Information"
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold)
+        Label11.Location = New Point(37, 101)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(73, 31)
+        Label11.TabIndex = 48
+        Label11.Text = "Male:"
+        ' 
+        ' Label13
+        ' 
+        Label13.AutoSize = True
+        Label13.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold)
+        Label13.Location = New Point(19, 61)
+        Label13.Name = "Label13"
+        Label13.Size = New Size(91, 31)
+        Label13.TabIndex = 7
+        Label13.Text = "Gender"
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold)
+        Label10.Location = New Point(14, 148)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(96, 31)
+        Label10.TabIndex = 49
+        Label10.Text = "Female:"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold)
+        Label9.Location = New Point(19, 196)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(129, 31)
+        Label9.TabIndex = 50
+        Label9.Text = "Age Group"
         ' 
         ' frmSummary
         ' 
-        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
         BackColor = Color.FromArgb(CByte(255), CByte(214), CByte(153))
-        ClientSize = New Size(1434, 978)
-        Controls.Add(Chart1)
+        ClientSize = New Size(1147, 782)
+        Controls.Add(Panel3)
+        Controls.Add(Panel2)
+        Controls.Add(Panel1)
+        Controls.Add(lblTitle)
         Controls.Add(PictureBox1)
         Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        Margin = New Padding(2)
         Name = "frmSummary"
         StartPosition = FormStartPosition.CenterScreen
         Text = "frmSummary"
         CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
-        CType(Chart1, ComponentModel.ISupportInitialize).EndInit()
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        Panel2.ResumeLayout(False)
+        Panel2.PerformLayout()
+        Panel3.ResumeLayout(False)
+        Panel3.PerformLayout()
         ResumeLayout(False)
+        PerformLayout()
     End Sub
     Friend WithEvents PictureBox1 As PictureBox
-    Friend WithEvents Chart1 As DataVisualization.Charting.Chart
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents cmbSitio As ComboBox
+    Friend WithEvents btnGenerate As Button
+    Friend WithEvents dtpTo As DateTimePicker
+    Friend WithEvents dtpFrom As DateTimePicker
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents MySqlCommand1 As MySql.Data.MySqlClient.MySqlCommand
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label16 As Label
+    Friend WithEvents lblHouseholds As Label
+    Friend WithEvents lblTotalResidents As Label
+    Friend WithEvents lblSitioName As Label
+    Friend WithEvents lblSenior As Label
+    Friend WithEvents lblAdulthood As Label
+    Friend WithEvents lblChildhood As Label
+    Friend WithEvents lblFemale As Label
+    Friend WithEvents lblMale As Label
 End Class
--- frmSummary.vb
+++ frmSummary.vb
@@ -3,64 +3,93 @@ Imports MySql.Data.MySqlClient
 
 Public Class frmSummary
 
-    Private Sub LoadResidentsByGender()
+    ' Connection String (edit this based on your DB settings)
+    Dim connStr As String = "server=localhost;userid=root;password=;database=tunasan_management_db;"
+    Dim cn As New MySqlConnection(connStr)
+
+    Private Sub frmSummary_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadSitios()
+    End Sub
+
+    Private Sub LoadSitios()
         Try
-            Call koneksyon()
+            cn.Open()
+            Dim cmd As New MySqlCommand("SELECT DISTINCT sitio_name FROM reports_summary", cn)
+            Dim reader As MySqlDataReader = cmd.ExecuteReader()
 
-            Dim query As String = "SELECT gender, COUNT(*) AS total FROM residents GROUP BY gender"
-            Dim cmd As New MySqlCommand(query, cn)
-            Dim dr As MySqlDataReader = cmd.ExecuteReader()
-
-            ' Clear previous chart data
-            Chart1.Series.Clear()
-            Chart1.Titles.Clear()
-            Chart1.ChartAreas.Clear()
-
-            ' Setup chart area
-            Dim area As New ChartArea()
-            area.AxisX.Title = "Gender"
-            area.AxisY.Title = "Number of Residents"
-            area.AxisX.Interval = 1
-            Chart1.ChartAreas.Add(area)
-
-            ' Create series for Male and Female
-            Dim seriesMale As New Series("Male")
-            seriesMale.ChartType = SeriesChartType.Column
-            seriesMale.Color = Color.Blue
-            seriesMale.IsValueShownAsLabel = True
-            seriesMale("PointWidth") = "0.4"
-            Chart1.Series.Add(seriesMale)
-
-            Dim seriesFemale As New Series("Female")
-            seriesFemale.ChartType = SeriesChartType.Column
-            seriesFemale.Color = Color.Pink
-            seriesFemale.IsValueShownAsLabel = True
-            seriesFemale("PointWidth") = "0.4"
-            Chart1.Series.Add(seriesFemale)
-
-            ' Add data points
-            While dr.Read()
-                Dim gender As String = dr("gender").ToString().Trim()
-                Dim total As Integer = Convert.ToInt32(dr("total"))
-
-                If gender = "Male" Then
-                    seriesMale.Points.AddXY("Male", total)
-                    seriesFemale.Points.AddXY("Male", 0) ' keep alignment
-                ElseIf gender = "Female" Then
-                    seriesFemale.Points.AddXY("Female", total)
-                    seriesMale.Points.AddXY("Female", 0) ' keep alignment
-                End If
+            cmbSitio.Items.Clear()
+            While reader.Read()
+                cmbSitio.Items.Add(reader("sitio_name").ToString())
             End While
-
-            ' Chart title
-            Chart1.Titles.Add("Residents by Gender")
+            reader.Close()
 
         Catch ex As Exception
-            MessageBox.Show("Error: " & ex.Message)
+            MessageBox.Show("Error loading sitios: " & ex.Message, "Database Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
         Finally
             cn.Close()
         End Try
     End Sub
 
+    Private Sub btnGenerate_Click(sender As Object, e As EventArgs) Handles btnGenerate.Click
+        If cmbSitio.Text = "" Then
+            MessageBox.Show("Please select a sitio first.", "Notice", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Return
+        End If
+
+        Try
+            cn.Open()
+
+            Dim query As String =
+                "SELECT sitio_name, " &
+                "SUM(total_residents) AS total_residents, " &
+                "SUM(households) AS households, " &
+                "SUM(male_count) AS male_count, " &
+                "SUM(female_count) AS female_count, " &
+                "SUM(childhood_count) AS childhood_count, " &
+                "SUM(adulthood_count) AS adulthood_count, " &
+                "SUM(senior_count) AS senior_count " &
+                "FROM reports_summary " &
+                "WHERE sitio_name = @sitio " &
+                "AND report_from >= @fromDate " &
+                "AND report_to <= @toDate " &
+                "GROUP BY sitio_name"
+
+            Dim cmd As New MySqlCommand(query, cn)
+            cmd.Parameters.AddWithValue("@sitio", cmbSitio.Text)
+            cmd.Parameters.AddWithValue("@fromDate", dtpFrom.Value.Date)
+            cmd.Parameters.AddWithValue("@toDate", dtpTo.Value.Date)
+
+            Dim reader As MySqlDataReader = cmd.ExecuteReader()
+
+            If reader.Read() Then
+                lblSitioName.Text = reader("sitio_name").ToString()
+                lblTotalResidents.Text = reader("total_residents").ToString()
+                lblHouseholds.Text = reader("households").ToString()
+
+                lblMale.Text = reader("male_count").ToString()
+                lblFemale.Text = reader("female_count").ToString()
+
+                lblChildhood.Text = reader("childhood_count").ToString()
+                lblAdulthood.Text = reader("adulthood_count").ToString()
+                lblSenior.Text = reader("senior_count").ToString()
+            Else
+                lblSitioName.Text = "0"
+                lblTotalResidents.Text = "0"
+                lblHouseholds.Text = "0"
+                lblMale.Text = "0"
+                lblFemale.Text = "0"
+                lblChildhood.Text = "0"
+                lblAdulthood.Text = "0"
+                lblSenior.Text = "0"
+            End If
+
+            reader.Close()
+
+        Catch ex As Exception
+            MessageBox.Show("Error generating report: " & ex.Message, "Database Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            cn.Close()
+        End Try
+    End Sub
 
 End Class
