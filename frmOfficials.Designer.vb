3120e30419d6a14cbd3c188b6bf92c937646e28b
Oliveros <lynetteoliveros02@gmail.com>
2025-11-04 14:51:48 +0800

    fff

--- frmOfficials.Designer.vb
+++ frmOfficials.Designer.vb
@@ -22,73 +22,57 @@ Partial Class frmOfficials
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim DataGridViewCellStyle1 As DataGridViewCellStyle = New DataGridViewCellStyle()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmOfficials))
         Panel1 = New Panel()
-        Button3 = New Button()
-        Button2 = New Button()
-        Button1 = New Button()
-        DataGridView1 = New DataGridView()
         Label2 = New Label()
+        Panel2 = New Panel()
+        Panel3 = New Panel()
+        Panel4 = New Panel()
+        Panel5 = New Panel()
+        Panel6 = New Panel()
+        Panel7 = New Panel()
+        Panel8 = New Panel()
+        Panel9 = New Panel()
+        Panel10 = New Panel()
+        Panel11 = New Panel()
+        Panel12 = New Panel()
+        Panel13 = New Panel()
+        Panel14 = New Panel()
+        Panel15 = New Panel()
+        Panel16 = New Panel()
+        Panel17 = New Panel()
+        Panel18 = New Panel()
+        Panel19 = New Panel()
         Panel1.SuspendLayout()
-        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' Panel1
         ' 
         Panel1.BackColor = Color.FromArgb(CByte(255), CByte(184), CByte(77))
-        Panel1.Controls.Add(Button3)
-        Panel1.Controls.Add(Button2)
-        Panel1.Controls.Add(Button1)
-        Panel1.Controls.Add(DataGridView1)
+        Panel1.Controls.Add(Panel19)
+        Panel1.Controls.Add(Panel17)
+        Panel1.Controls.Add(Panel18)
+        Panel1.Controls.Add(Panel16)
+        Panel1.Controls.Add(Panel15)
+        Panel1.Controls.Add(Panel14)
+        Panel1.Controls.Add(Panel13)
+        Panel1.Controls.Add(Panel12)
+        Panel1.Controls.Add(Panel11)
+        Panel1.Controls.Add(Panel10)
+        Panel1.Controls.Add(Panel9)
+        Panel1.Controls.Add(Panel8)
+        Panel1.Controls.Add(Panel7)
+        Panel1.Controls.Add(Panel6)
+        Panel1.Controls.Add(Panel5)
+        Panel1.Controls.Add(Panel4)
+        Panel1.Controls.Add(Panel3)
+        Panel1.Controls.Add(Panel2)
         Panel1.Controls.Add(Label2)
-        Panel1.Location = New Point(22, 35)
-        Panel1.Margin = New Padding(2, 2, 2, 2)
+        Panel1.Location = New Point(19, 26)
+        Panel1.Margin = New Padding(2)
         Panel1.Name = "Panel1"
-        Panel1.Size = New Size(1095, 722)
+        Panel1.Size = New Size(1511, 754)
         Panel1.TabIndex = 0
-        ' 
-        ' Button3
-        ' 
-        Button3.Location = New Point(879, 66)
-        Button3.Margin = New Padding(2, 2, 2, 2)
-        Button3.Name = "Button3"
-        Button3.Size = New Size(186, 60)
-        Button3.TabIndex = 5
-        Button3.Text = "Button3"
-        Button3.UseVisualStyleBackColor = True
-        ' 
-        ' Button2
-        ' 
-        Button2.Location = New Point(688, 66)
-        Button2.Margin = New Padding(2, 2, 2, 2)
-        Button2.Name = "Button2"
-        Button2.Size = New Size(186, 60)
-        Button2.TabIndex = 4
-        Button2.Text = "Button2"
-        Button2.UseVisualStyleBackColor = True
-        ' 
-        ' Button1
-        ' 
-        Button1.Location = New Point(497, 66)
-        Button1.Margin = New Padding(2, 2, 2, 2)
-        Button1.Name = "Button1"
-        Button1.Size = New Size(186, 60)
-        Button1.TabIndex = 3
-        Button1.Text = "Add Official"
-        Button1.UseVisualStyleBackColor = True
-        ' 
-        ' DataGridView1
-        ' 
-        DataGridViewCellStyle1.BackColor = Color.FromArgb(CByte(255), CByte(184), CByte(77))
-        DataGridView1.AlternatingRowsDefaultCellStyle = DataGridViewCellStyle1
-        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        DataGridView1.Location = New Point(25, 140)
-        DataGridView1.Margin = New Padding(2, 2, 2, 2)
-        DataGridView1.Name = "DataGridView1"
-        DataGridView1.RowHeadersWidth = 62
-        DataGridView1.Size = New Size(1041, 554)
-        DataGridView1.TabIndex = 2
         ' 
         ' Label2
         ' 
@@ -98,32 +82,189 @@ Partial Class frmOfficials
         Label2.Location = New Point(0, 0)
         Label2.Margin = New Padding(2, 0, 2, 0)
         Label2.Name = "Label2"
-        Label2.Size = New Size(377, 54)
+        Label2.Size = New Size(301, 45)
         Label2.TabIndex = 1
         Label2.Text = "Barangay Officials"
         ' 
+        ' Panel2
+        ' 
+        Panel2.BackColor = Color.Gold
+        Panel2.Location = New Point(593, 17)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(236, 127)
+        Panel2.TabIndex = 2
+        ' 
+        ' Panel3
+        ' 
+        Panel3.BackColor = Color.Gold
+        Panel3.Location = New Point(16, 339)
+        Panel3.Name = "Panel3"
+        Panel3.Size = New Size(236, 82)
+        Panel3.TabIndex = 3
+        ' 
+        ' Panel4
+        ' 
+        Panel4.BackColor = Color.Gold
+        Panel4.Location = New Point(323, 210)
+        Panel4.Name = "Panel4"
+        Panel4.Size = New Size(236, 82)
+        Panel4.TabIndex = 3
+        ' 
+        ' Panel5
+        ' 
+        Panel5.BackColor = Color.Gold
+        Panel5.Location = New Point(323, 368)
+        Panel5.Name = "Panel5"
+        Panel5.Size = New Size(236, 313)
+        Panel5.TabIndex = 3
+        ' 
+        ' Panel6
+        ' 
+        Panel6.BackColor = Color.Gold
+        Panel6.Location = New Point(586, 365)
+        Panel6.Name = "Panel6"
+        Panel6.Size = New Size(236, 313)
+        Panel6.TabIndex = 4
+        ' 
+        ' Panel7
+        ' 
+        Panel7.BackColor = Color.Gold
+        Panel7.Location = New Point(966, 229)
+        Panel7.Name = "Panel7"
+        Panel7.Size = New Size(236, 313)
+        Panel7.TabIndex = 5
+        ' 
+        ' Panel8
+        ' 
+        Panel8.BackColor = Color.Gold
+        Panel8.Location = New Point(1248, 230)
+        Panel8.Name = "Panel8"
+        Panel8.Size = New Size(236, 313)
+        Panel8.TabIndex = 5
+        ' 
+        ' Panel9
+        ' 
+        Panel9.BackColor = Color.Black
+        Panel9.Location = New Point(699, 145)
+        Panel9.Name = "Panel9"
+        Panel9.Size = New Size(12, 221)
+        Panel9.TabIndex = 3
+        ' 
+        ' Panel10
+        ' 
+        Panel10.BackColor = Color.Black
+        Panel10.Location = New Point(711, 174)
+        Panel10.Name = "Panel10"
+        Panel10.Size = New Size(660, 13)
+        Panel10.TabIndex = 4
+        ' 
+        ' Panel11
+        ' 
+        Panel11.BackColor = Color.Black
+        Panel11.Location = New Point(1078, 186)
+        Panel11.Name = "Panel11"
+        Panel11.Size = New Size(10, 42)
+        Panel11.TabIndex = 4
+        ' 
+        ' Panel12
+        ' 
+        Panel12.BackColor = Color.Black
+        Panel12.Location = New Point(1361, 186)
+        Panel12.Name = "Panel12"
+        Panel12.Size = New Size(10, 42)
+        Panel12.TabIndex = 5
+        ' 
+        ' Panel13
+        ' 
+        Panel13.BackColor = Color.Black
+        Panel13.Location = New Point(431, 322)
+        Panel13.Name = "Panel13"
+        Panel13.Size = New Size(280, 10)
+        Panel13.TabIndex = 5
+        ' 
+        ' Panel14
+        ' 
+        Panel14.BackColor = Color.Black
+        Panel14.Location = New Point(433, 327)
+        Panel14.Name = "Panel14"
+        Panel14.Size = New Size(10, 42)
+        Panel14.TabIndex = 5
+        ' 
+        ' Panel15
+        ' 
+        Panel15.BackColor = Color.Black
+        Panel15.Location = New Point(425, 174)
+        Panel15.Name = "Panel15"
+        Panel15.Size = New Size(300, 10)
+        Panel15.TabIndex = 5
+        ' 
+        ' Panel16
+        ' 
+        Panel16.BackColor = Color.Black
+        Panel16.Location = New Point(425, 174)
+        Panel16.Name = "Panel16"
+        Panel16.Size = New Size(10, 37)
+        Panel16.TabIndex = 5
+        ' 
+        ' Panel17
+        ' 
+        Panel17.BackColor = Color.Black
+        Panel17.Location = New Point(136, 301)
+        Panel17.Name = "Panel17"
+        Panel17.Size = New Size(299, 15)
+        Panel17.TabIndex = 6
+        ' 
+        ' Panel18
+        ' 
+        Panel18.BackColor = Color.Black
+        Panel18.Location = New Point(129, 301)
+        Panel18.Name = "Panel18"
+        Panel18.Size = New Size(10, 37)
+        Panel18.TabIndex = 6
+        ' 
+        ' Panel19
+        ' 
+        Panel19.BackColor = Color.Black
+        Panel19.Location = New Point(425, 292)
+        Panel19.Name = "Panel19"
+        Panel19.Size = New Size(10, 20)
+        Panel19.TabIndex = 6
+        ' 
         ' frmOfficials
         ' 
-        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
         BackgroundImageLayout = ImageLayout.Stretch
-        ClientSize = New Size(1777, 1055)
+        ClientSize = New Size(1555, 791)
         Controls.Add(Panel1)
         DoubleBuffered = True
-        Margin = New Padding(2, 2, 2, 2)
+        Margin = New Padding(2)
         Name = "frmOfficials"
         Text = "frmOfficials"
         Panel1.ResumeLayout(False)
         Panel1.PerformLayout()
-        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
     End Sub
 
     Friend WithEvents Panel1 As Panel
     Friend WithEvents Label2 As Label
-    Friend WithEvents DataGridView1 As DataGridView
-    Friend WithEvents Button3 As Button
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Button1 As Button
+    Friend WithEvents Panel12 As Panel
+    Friend WithEvents Panel11 As Panel
+    Friend WithEvents Panel10 As Panel
+    Friend WithEvents Panel9 As Panel
+    Friend WithEvents Panel8 As Panel
+    Friend WithEvents Panel7 As Panel
+    Friend WithEvents Panel6 As Panel
+    Friend WithEvents Panel5 As Panel
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel19 As Panel
+    Friend WithEvents Panel17 As Panel
+    Friend WithEvents Panel18 As Panel
+    Friend WithEvents Panel16 As Panel
+    Friend WithEvents Panel15 As Panel
+    Friend WithEvents Panel14 As Panel
+    Friend WithEvents Panel13 As Panel
 End Class
--- frmOfficials.vb
+++ frmOfficials.vb
@@ -1,24 +1,5 @@
 ﻿Imports MySql.Data.MySqlClient
 
 Public Class frmOfficials
-    Private Sub frmOfficials_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        LoadOfficials()
-    End Sub
 
-    Private Sub LoadOfficials()
-        Try
-            koneksyon()
-            Dim query As String = "SELECT official_id, full_name, position, term_start, term_end FROM officials ORDER BY position"
-            cmd = New MySqlCommand(query, cn)
-            da = New MySqlDataAdapter(cmd)
-            dt = New DataTable()
-            da.Fill(dt)
-
-            DataGridView1.DataSource = dt
-            DataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
-
-        Catch ex As Exception
-            MessageBox.Show("Error loading officials: " & ex.Message)
-        End Try
-    End Sub
 End Class
